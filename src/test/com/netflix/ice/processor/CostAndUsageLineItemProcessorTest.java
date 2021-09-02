c336b6769b5dc1bb2263a39eb5df27a28bd492e9
jimroth <jimroth@users.noreply.github.com>
2021-09-02 09:58:18 -0700

    Add unit test for IncludeZeroCostUsageForProducts property

--- src/test/com/netflix/ice/processor/CostAndUsageLineItemProcessorTest.java
+++ src/test/com/netflix/ice/processor/CostAndUsageLineItemProcessorTest.java
@@ -140,7 +140,7 @@ public class CostAndUsageLineItemProcessorTest {
 			reservationService.injectReservation(reservation);
     	
     	resourceService.initHeader(lineItem.getResourceTagsHeader(), account1);
-    	return new CostAndUsageReportLineItemProcessor(accountService, productService, reservationService, resourceService, null);
+    	return new CostAndUsageReportLineItemProcessor(accountService, productService, reservationService, resourceService, new String[]{"AmazonCloudFront"});
     }
     
     private ReformedMetaData testReform(Line line, PurchaseOption purchaseOption) throws IOException {
@@ -151,7 +151,7 @@ public class CostAndUsageLineItemProcessorTest {
     
     @Test
     public void testGetRegion() throws IOException {
-        CostAndUsageReportLineItemProcessor lineItemProcessor = new CostAndUsageReportLineItemProcessor(accountService, productService, null, resourceService, null);
+        CostAndUsageReportLineItemProcessor lineItemProcessor = new CostAndUsageReportLineItemProcessor(accountService, productService, null, resourceService, new String[]{"AmazonCloudFront"});
         LineItem lineItem = newCurLineItem(manifest2017, null);
 
         // Test case where region is in usage-type prefix
@@ -1360,4 +1360,15 @@ public class CostAndUsageLineItemProcessorTest {
         };
         test.run("2021-03-01T00:00:00Z", expected);
     }
+
+    @Test
+	public void testZeroCostCloudFront() throws Exception {
+    	Line line = new Line(LineItemType.Usage, "us-east-1", "", "Amazon CloudFront", "US-Requests-Tier1", "GET", "CloudFront Requests", PricingTerm.none, "2021-08-01T00:00:00Z", "2021-08-01T01:00:00Z", "1000000", "0", "");
+    	ProcessTest test = new ProcessTest(line, Result.hourly, 31);
+    	Datum[] expected = {
+    			new Datum(a2, Region.US_EAST_1, null, productService.getProduct(Product.Code.CloudFront), Operation.getOperation("GET"), "US-Requests-Tier1", 0.0, 1000000),
+		};
+    	test.run("2021-08-01T00:00:00Z", expected);
+	}
+
 }
